b009c7f7222c36f84f97186d3a47e6721cdd3fb0
Juditga97 <judit.ga97@gmail.com>
2026-04-07 19:28:58 +0200

    Update

--- model.py
+++ model.py
@@ -1,13 +1,19 @@
-import joblib #Lo usamos para cargar modelos entrenados guardados en archivos .pkl
-import numpy as np
+import joblib
 import pandas as pd
+import os
 
-model = joblib.load("heart_attack_model_01.pkl") #Cargamos el modelo
+# Obtener la ruta del directorio actual
+BASE_DIR = os.path.dirname(__file__)
+
+# Construir la ruta completa del modelo
+model_path = os.path.join(BASE_DIR, "heart_attack_model_01.pkl")
+
+# Cargar el modelo
+model = joblib.load(model_path)
 
 def predict(features: dict):
 
-    df = pd.DataFrame([features])      
-
+    df = pd.DataFrame([features])
 
     prediction = model.predict(df)
 
