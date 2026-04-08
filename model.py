5d8b86aed57002dfb3c6fa2c218d3ffc61670d05
cflorenciafiore <camiffiore@gmail.com>
2026-04-08 06:48:03 +0200

    gunicorn app:app --workers=1 --threads=1

--- model.py
+++ model.py
@@ -2,35 +2,27 @@ import joblib
 import pandas as pd
 import os
 
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 model_path = os.path.join(BASE_DIR, "heart_attack_model_01.pkl")
 
-model = None
+# Cargar modelo UNA VEZ al inicio
+model = joblib.load(model_path)
 
-# orden correcto de columnas
 columns = [
-"State","Sex","GeneralHealth","PhysicalHealthDays","MentalHealthDays",
-"LastCheckupTime","PhysicalActivities","SleepHours","RemovedTeeth",
-"HadAngina","HadStroke","HadCOPD","HadDiabetes","HadKidneyDisease",
-"HadArthritis","HadSkinCancer","AlcoholDrinkers","SmokerStatus",
-"DifficultyWalking","DifficultyDressingBathing","DifficultyErrands",
-"DeafOrHardOfHearing","BlindOrVisionDifficulty","DifficultyConcentrating",
-"AgeCategory","WeightInKilograms","CovidPos","ChestScan","PneumoVaxEver"
+    "State","Sex","GeneralHealth","PhysicalHealthDays","MentalHealthDays",
+    "LastCheckupTime","PhysicalActivities","SleepHours","RemovedTeeth",
+    "HadAngina","HadStroke","HadCOPD","HadDiabetes","HadKidneyDisease",
+    "HadArthritis","HadSkinCancer","AlcoholDrinkers","SmokerStatus",
+    "DifficultyWalking","DifficultyDressingBathing","DifficultyErrands",
+    "DeafOrHardOfHearing","BlindOrVisionDifficulty","DifficultyConcentrating",
+    "AgeCategory","WeightInKilograms","CovidPos","ChestScan","PneumoVaxEver"
 ]
 
 def predict(features: dict):
 
-    global model
-
-    if model is None:
-        model = joblib.load(model_path)
-
     df = pd.DataFrame([features])
-
-    # asegurar orden correcto
     df = df[columns]
 
     prediction = model.predict(df)
 
     return int(prediction[0])
-
